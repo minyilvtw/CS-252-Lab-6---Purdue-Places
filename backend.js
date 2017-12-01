8dd529f84db8160a9f5241c21532cd94cbe86c80
Ryan Miller <mill1514@purdue.edu>
2017-11-30 19:02:16 -0500

    Add files via upload
    I did some work. It works now.

---
+++ backend.js
@@ -0,0 +1,73 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyD3cGPVywJtRnm4vQMtwRM9DVQloAmyCso",
+    authDomain: "boiler-canvas.firebaseapp.com",
+    databaseURL: "https://boiler-canvas.firebaseio.com",
+    projectId: "boiler-canvas",
+    storageBucket: "boiler-canvas.appspot.com",
+    messagingSenderId: "985213101820"
+  };
+
+  firebase.initializeApp(config);
+
+  var x_max = 120;
+  var y_max = 80;
+  var map = null; // This is where pixel data is stored locally
+	
+  /* Gets the entire canvas data in string form */
+  function getCanvas() {  
+
+    firebase.database().ref('map/').once('value').then(function(snapshot) {
+      map = snapshot.val().pixels;
+      colorCanvas(map);
+      //console.log(map);
+    });
+
+  }
+
+  function getMap() {
+  	return map;
+  }
+
+  /* Sets the entire canvas */
+  function setCanvas(canv) {
+	  
+	  /* Set the canvas as the given string */
+  	firebase.database().ref("map/").set({
+		  pixels: canv
+  	});
+
+  }
+
+  /* Sets a pixel of the canvas */
+  function setPixel(x, y, val) {
+	  
+	  /* Get index of pixel to be changed */
+    var index = (x_max * y) + x - 1;
+	  
+    /* Get current firebase map */
+	  firebase.database().ref('map/').once('value').then(function(snapshot) {
+      map = snapshot.val().pixels;
+
+      console.log("map["+index+"] used to be: "+ map[index]);
+
+      console.log("Setting to "+val+"...")
+
+      /* Set index to new val */
+      map = map.replaceAt(index, val.toString());
+
+      console.log("Now: map["+index+"] is: "+ map[index]);
+
+      /* Update firebase */
+      setCanvas(map);
+
+      /* Update our canvas */
+      colorCanvas(map);
+    });
+
+  }
+
+  String.prototype.replaceAt=function(index, replacement) {
+    return this.substr(0, index) + replacement + this.substr(index + replacement.length);
+  }
--- canvas.js
+++ canvas.js
@@ -4,75 +4,103 @@ document.addEventListener("click", mouseClick);
 
 var canvas = document.querySelector('canvas');
 
-canvas.width = 440;
-canvas.height = 400;
+canvas.width = 650; 	// 120 'px' + 10 'px' (pallette)
+canvas.height = 400; 	// 80  'px'
+var pixWidth = 120;
+var pixHeight = 80;
 
 var context = canvas.getContext('2d');
-var color = ['rgb(0,0,0)'
-					,'rgb(255,0,0)'
-					,'rgb(0,255,0)'
-					,'rgb(0,0,255)'];
-var colorIndex = 0;
-
-var x,y;
+var currColor = 0;
 var canPaint = true;
+var color = ['rgb(0,0,0)' // Black
+					,'rgb(255,0,0)' // Red
+					,'rgb(0,255,0)' // Green
+					,'rgb(0,0,255)' // Blue
+ 					,'rgb(128,0,0)' // Maroon?
+ 					,'rgb(255,255,255)']; // White
 
-for(var i = 0; i < 4; i ++){
-	context.fillStyle = color[i];
-	context.fillRect(canvas.width-40,i*40,40,40);
+function initializeCanvas() {
+	/* Filling in color pallete on right */
+	for(var i = 0; i < 5; i ++){
+		context.fillStyle = color[i];
+		context.fillRect(canvas.width-40,i*40,40,40);
+	}
+
+	/* Filling in empty space on color pallette */
+	for(var i = 5; i < 20; i ++){
+		context.fillStyle = 'rgba(0,0,0,0.5)';
+		context.fillRect(canvas.width-40,i*40,40,40);
+	}
+
+	context.fillStyle = color[0];
+
+	setInterval(enablePaint, 3000);
 }
-
-for(var i = 4; i < 20; i ++){
-	context.fillStyle = 'rgba(0,0,0,0.5)';
-	context.fillRect(canvas.width-40,i*40,40,40);
-}
-
-context.fillStyle = color[0];
-
-setInterval(enablePaint, 3000);
 
 function mouseClick(event) {
-	// minus margin to get click position
-    x = event.clientX - 15;
-    y = event.clientY - 15;
-    //console.log("first:"+x+" "+y);
-    y = Math.round((y*10)/10);
-    if(x % 10 < 5){
-    	x = Math.floor(x / 10) * 10;
-    } else {
-    	x = Math.ceil(x / 10) * 10;
-    }
-    if(y % 10 < 5){
-    	y = Math.floor(y / 10) * 10;
-    } else {
-    	y = Math.ceil(y / 10) * 10;
-    }
+
+	/* Get mouse position */
+	var pos = getMousePos(event);
+
     //console.log("second:"+x+" "+y);
-    if(x > 0 && x < canvas.width - 40){
-    	if(canPaint){
-    		paint(x, y, color);
+    if ((pos.x > 0 && pos.y > 0) && (pos.x < canvas.width - 40 && pos.y < canvas.height)) {
+	    
+    	if (canPaint) {
+    		setPixel(round(pos.x/5, 0), round(pos.y/5, 0), currColor);
     		canPaint = false;
+			console.log("DRAW: (" + round(pos.x/5, 0) + ", " + round(pos.y/5, 0) + ")");
     	} else {
     		console.log("WAIT");
     	}
+	    
 	} else {
-		if(y < 160){
-			pickColor(x,y,color);
+		if (pos.y < 160) {
+			pickColor(pos.x, pos.y);
 		}
 	}
 }
 
-function paint(x, y, color){
+/* paint pixel given color (should be int) */
+function paint(x, y, colorIndex){
+	context.fillStyle = color[colorIndex];
 	context.fillRect(x,y,10,10);
-	console.log("DRAW");
 }
 
-function pickColor(x, y, color){
-	var choice = Math.round((y/40)) -1 ;
-	console.log("index"+ choice);
-	context.fillStyle = color[choice];
+/* Get index from x, y */
+function pickColor(x, y){
+	currColor = Math.round((y/40)) - 1;
+}
+
+/* Colors the entire canvas using the 'canv' string */
+function colorCanvas(canv){
+
+	for (var i = 0; i < pixHeight; i++) {
+		for (var j = 0; j < pixWidth; j++) {
+
+			var index = (i * pixWidth) + j;
+			paint(j * 5, i * 5, parseInt(canv[index]));
+
+		}
+	}
 }
 
 function enablePaint(){
 	canPaint = true;
 }
+
+function getMousePos(event) {
+
+	var rect = canvas.getBoundingClientRect();
+
+	return {
+      x: round(event.clientX - rect.left + 2, 0),
+      y: round(event.clientY - rect.top - 3, 0)
+    };
+}
+
+function round(value, decimals) {
+  	return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+}
+
+initializeCanvas();
+
