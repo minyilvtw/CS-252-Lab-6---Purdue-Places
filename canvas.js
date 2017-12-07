48c20c02c84451580b9225a36ed56f3b2470089f
Arnav Jain <jain150@purdue.edu>
2017-12-07 17:27:42 -0500

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -16,8 +16,8 @@ var color = ['rgb(0,0,0)', // Black
 	     'rgb(136,136,136)', // Gray
 	     'rgb(255,255,255)', // White
 	     'rgb(229,0,0)', // Red
+	     'rgb(255,165,0)', // Orange
 	     'rgb(194,142,14)', // Purdue Gold
-	     'rgb(160,106,66)', // Brown
 	     'rgb(229,217,0)', // Yellow
 	     'rgb(2,190,1)', // Green
 	     'rgb(0,0,234)', // Blue
