4266be62e1f0e65386985c50db40bb388c90b5b4
Ryan Miller <mill1514@purdue.edu>
2017-12-07 20:34:41 -0800

    Update canvas.js

--- canvas.js
+++ canvas.js
@@ -101,7 +101,7 @@ function colorCanvas(canv){
 
 function delayEnablePaint() {
 	console.log("Start Timer...");
-    setTimeout(enablePaint, 1);
+    setTimeout(enablePaint, 2000);
 }
 
 function enablePaint(){
